2a4f3366ae1f50572322cf0ce17ac53396389e6b
YBoah <yawboahene.yb@gmail.com>
2024-05-20 18:37:27 +0000

    Added the characters count functionality

--- word-analytics/src/components/Stats.jsx
+++ word-analytics/src/components/Stats.jsx
@@ -1,10 +1,10 @@
 
 
-function Stats() {
+function Stats({numberOfCharacters}) {
   return (
     <section className="stats">
       <Stat number={0} label="Words"  />
-      <Stat number={0} label="Characters"/>
+      <Stat number={numberOfCharacters} label="Characters"/>
       <Stat number={280} label="Instagram"/>
       <Stat number={2200} label="Facebook"/>
     </section>
@@ -17,7 +17,7 @@ export default Stats
 function Stat({number, label}) {
   return (
     <section className="stat">
-      <span className="stat_number">{number}</span>
+      <span className="stat__number">{number}</span>
       <h2 className="second-heading">{label}</h2>
     </section>
   );
