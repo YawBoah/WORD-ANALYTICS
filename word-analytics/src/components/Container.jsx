280a8124becfdf61e4312557808639f3aa569714
YBoah <yawboahene.yb@gmail.com>
2024-05-20 19:04:00 +0000

    Restructuring of the stats component

--- word-analytics/src/components/Container.jsx
+++ word-analytics/src/components/Container.jsx
@@ -1,24 +1,23 @@
 import { useState } from "react";
-import Stats from "./Stats"
-import Textarea from "./Textarea"
+import Stats from "./Stats";
+import Textarea from "./Textarea";
 
 function Container() {
   const [text, setText] = useState("");
-const numberOfWords = text.split(/\s/).filter((word) => word !== "").length 
-  const numberOfCharacters = text.length;
-  const instagramCharactersLeft = 280 - numberOfCharacters;
-  const facebookCharactersLeft = 2200 - numberOfCharacters;
+ 
+  const stats = {
+    numberOfWords: text.split(/\s/).filter((word) => word !== "").length,
+    numberOfCharacters: text.length,
+    instagramCharactersLeft: 200 - text.length,
+    facebookCharactersLeft: 2200 - text.length,
+  };
 
   return (
     <main className="container">
-   <Textarea text={text} setText={setText}/>
-   <Stats 
-   numberOfWords={numberOfWords}
-   numberOfCharacters={numberOfCharacters}
-   instagramCharactersLeft={instagramCharactersLeft} 
-   facebookCharactersLeft={facebookCharactersLeft}/>
+      <Textarea text={text} setText={setText} />
+      <Stats stats={stats} />
     </main>
-  )
+  );
 }
 
-export default Container
+export default Container;
--- word-analytics/src/components/Stats.jsx
+++ word-analytics/src/components/Stats.jsx
@@ -1,20 +1,17 @@
-
-
-function Stats({numberOfWords, numberOfCharacters, instagramCharactersLeft,facebookCharactersLeft}) {
+function Stats({ stats }) {
   return (
     <section className="stats">
-      <Stat number={numberOfWords} label="Words"  />
-      <Stat number={numberOfCharacters} label="Characters"/>
-      <Stat number={instagramCharactersLeft} label="Instagram"/>
-      <Stat number={facebookCharactersLeft} label="Facebook"/>
+      <Stat number={stats.numberOfWords} label="Words" />
+      <Stat number={stats.numberOfCharacters} label="Characters" />
+      <Stat number={stats.instagramCharactersLeft} label="Instagram" />
+      <Stat number={stats.facebookCharactersLeft} label="Facebook" />
     </section>
   );
 }
 
-export default Stats
+export default Stats;
 
-
-function Stat({number, label}) {
+function Stat({ number, label }) {
   return (
     <section className="stat">
       <span className="stat__number">{number}</span>
