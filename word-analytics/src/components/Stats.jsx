6aadff7f64e5a7d082b7e274d56538bf2aad73d5
YBoah <yawboahene.yb@gmail.com>
2024-05-20 11:05:19 +0000

    Making the Stats section reuseable

--- word-analytics/src/components/Stats.jsx
+++ word-analytics/src/components/Stats.jsx
@@ -3,9 +3,22 @@
 function Stats() {
   return (
     <section className="stats">
-
+      <Stat number={0} label="Words"  />
+      <Stat number={0} label="Characters"/>
+      <Stat number={280} label="Instagram"/>
+      <Stat number={2200} label="Facebook"/>
     </section>
-  )
+  );
 }
 
 export default Stats
+
+
+function Stat({number, label}) {
+  return (
+    <section className="stat">
+      <span className="stat_number">{number}</span>
+      <h2 className="second-heading">{label}</h2>
+    </section>
+  );
+}
